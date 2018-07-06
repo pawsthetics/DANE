2638f00a100273ea68cfe7b91935fb2c40b708fa
stephendov <40470740+stephendov@users.noreply.github.com>
2018-07-06 10:49:00 -0500

    sum5

--- js/import2.js
+++ js/import2.js
@@ -86,7 +86,7 @@ function errorHandler(evt) {
     
 //---------------------------------------------------------------------------
 
-object.onclick = analyze(){
+object.onclick = function(analyze){
 
 var loc1array = by_location[row2[0]];
 
